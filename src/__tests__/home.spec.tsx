b12fc60d75a0da671f12d13ce1faae10a99fd4cd
Aleydon <aleydonroberto@gmail.com>
2025-01-19 13:07:28 -0300

    ADD: home component render test + eslint rule update

--- src/__tests__/home.spec.tsx
+++ src/__tests__/home.spec.tsx
@@ -3,7 +3,11 @@ import { render } from '@testing-library/react-native';
 import Home from 'src/app/(tabs)';
 
 describe('Home Test', () => {
-  it('should render App', () => {
+  it('should render Home', () => {
+    const homeComponent = render(<Home />);
+    expect(homeComponent).toBeDefined();
+  });
+  it('should get text on screen', () => {
     const { getByText } = render(<Home />);
     const text = getByText('React Native Template');
     expect(text).toBeDefined();
