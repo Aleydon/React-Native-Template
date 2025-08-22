01ff62334a9c1b1c6c6f1d3d7826cc1e9f793237
Aleydon <aleydonroberto@gmail.com>
2025-08-22 20:01:13 -0300

    fix(setup): nativewind is working now

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable no-undef */
+
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: './src/app/global.css' });
--- metro.config.mjs
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-
-import { getDefaultConfig } from 'expo/metro-config';
-import { withNativeWind } from 'nativewind/metro';
-
-const config = getDefaultConfig(__dirname);
-
-export default withNativeWind(config, { input: './src/app/global.css' });
