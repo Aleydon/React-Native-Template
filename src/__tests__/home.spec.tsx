5f82e20fddb2f7657a2a196fd35b495686507ccd
Aleydon <aleydonroberto@gmail.com>
2026-08-10 21:33:40 -0300

    fix(test): fix server component test

--- src/__tests__/home.spec.tsx
+++ src/__tests__/home.spec.tsx
@@ -2,14 +2,19 @@ import { render } from '@testing-library/react-native';
 
 import Home from '@/app/(tabs)';
 
+jest.mock('@/actions/getServerData', () => ({
+  getServerData: jest.fn().mockResolvedValue('Hello from Server Functions!')
+}));
+
 describe('Home Test', () => {
-  it('should render Home', () => {
+  it('should render Home', async () => {
     const homeComponent = render(<Home />);
+    await homeComponent.findByText('Expo RSC & SSR Template');
     expect(homeComponent).toBeDefined();
   });
-  it('should get text on screen', () => {
-    const { getByText } = render(<Home />);
-    const text = getByText('React Native Template');
+  it('should get text on screen', async () => {
+    const { findByText } = render(<Home />);
+    const text = await findByText('Expo RSC & SSR Template');
     expect(text).toBeDefined();
   });
 });
